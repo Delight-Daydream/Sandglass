5510e3f86b5e3ea90fc9b6e3b9005698f6be9a9d
root <root@localhost.localdomain>
2019-12-09 04:05:04 -0500

    read

--- test.py
+++ test.py
@@ -1,21 +1,31 @@
 # import tensorflow as tf
 
+import struct
+
 class image_learn:
     def __init__(self, ipath="./lab/t10k-images-idx3-ubyte", lpath="./lab/t10k-labels-idx1-ubyte"):
         self.ipath = ipath
         self.lpath = lpath
         self.ifile = open(self.ipath, "rb")
+
+        self.imagic = self.ifile.read(4)
+        self.icount = self.ifile.read(4)
+        self.irows = self.ifile.read(4)
+        self.icols = self.ifile.read(4)
+
         self.lfile = open(self.lpath, "rb")
-        self.idata = self.ifile.read()
-        self.ldata = self.lfile.read()
-        self.ifile.close()
-        self.lfile.close()
+        self.lmagic = self.lfile.read(4)
+        self.lcount = self.lfile.read(4)
 
     def dump(self):
         print(self.ipath)
         print(self.lpath)
-        print(self.idata.count)
-        print(self.ldata.count)
+        print(int.from_bytes(self.imagic, 'big'))
+        print(int.from_bytes(self.icount, 'big'))
+        print(int.from_bytes(self.irows, 'big'))
+        print(int.from_bytes(self.icols, 'big'))
+        print(int.from_bytes(self.lmagic, 'big'))
+        print(int.from_bytes(self.lcount, 'big'))
 
 #ifile = open("./lab/t10k-images-idx3-ubyte", "rb")
 #lfile = open("./lab/t10k-labels-idx1-ubyte", "rb")
