d678f6bbb4190f2f0bb14026e6b726c7c2dda74c
root <root@localhost.localdomain>
2019-12-09 04:26:34 -0500

    ??

--- test.py
+++ test.py
@@ -1,21 +1,31 @@
-# import tensorflow as tf
-
+import tensorflow as tf
 import struct
 
 class image_learn:
-    def __init__(self, ipath="./lab/t10k-images-idx3-ubyte", lpath="./lab/t10k-labels-idx1-ubyte"):
+    def __init__(self, ipath="./lab/t10k-images-idx3-ubyte", lpath="./lab/t10k-labels-idx1-ubyte", logpath="./log"):
         self.ipath = ipath
         self.lpath = lpath
-        self.ifile = open(self.ipath, "rb")
+        self.logpath = logpath
 
+        self.ifile = open(self.ipath, "rb")
         self.imagic = self.ifile.read(4)
         self.icount = self.ifile.read(4)
         self.irows = self.ifile.read(4)
         self.icols = self.ifile.read(4)
+        self.unitsize = self.irows * self.icols
 
         self.lfile = open(self.lpath, "rb")
         self.lmagic = self.lfile.read(4)
         self.lcount = self.lfile.read(4)
+
+    def tfinit(self):
+        self.sess = tf.Session()
+        self.Input = tf.placeholder(tf.uint8, [None, self.unitsize])
+        self.W = tf.Variable(tf.zeros[self.unitsize, 10])
+        self.b = tf.Variable(tf.zeros[10])
+
+    def learn(self):
+        data = self.ifile.read(self.unitsize)
 
     def dump(self):
         print(self.ipath)
@@ -33,3 +43,5 @@ class image_learn:
 a = image_learn()
 
 a.dump()
+
+a.tfinit()
