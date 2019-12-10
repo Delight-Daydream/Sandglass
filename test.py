4ebbcd3cd54b01457319cd5e4a50cee5cfa83b87
root <root@localhost.localdomain>
2019-12-10 04:28:51 -0500

    a

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-import struct
+import numpy as np
 
 class image_learn:
     def __init__(self, ipath="./lab/t10k-images-idx3-ubyte", lpath="./lab/t10k-labels-idx1-ubyte", logpath="./log"):
@@ -9,9 +9,9 @@ class image_learn:
 
         self.ifile = open(self.ipath, "rb")
         self.imagic = self.ifile.read(4)
-        self.icount = self.ifile.read(4)
-        self.irows = self.ifile.read(4)
-        self.icols = self.ifile.read(4)
+        self.icount = int.from_bytes(self.ifile.read(4), 'big')
+        self.irows = int.from_bytes(self.ifile.read(4), 'big')
+        self.icols = int.from_bytes(self.ifile.read(4), 'big')
         self.unitsize = self.irows * self.icols
 
         self.lfile = open(self.lpath, "rb")
@@ -19,21 +19,63 @@ class image_learn:
         self.lcount = self.lfile.read(4)
 
     def tfinit(self):
+#        self.saver = tf.train.Saver(tf.global_variables())
         self.sess = tf.Session()
-        self.Input = tf.placeholder(tf.uint8, [None, self.unitsize])
-        self.W = tf.Variable(tf.zeros[self.unitsize, 10])
-        self.b = tf.Variable(tf.zeros[10])
+        self.ckpt = tf.train.get_checkpoint_state('./model')
+        if self.ckpt and tf.train.checkpoint_exists(self.ckpt.model_checkpoint_path):
+            self.saver.restore(self.sess, self.ckpt.model_checkpoint_path)
+        else:
+            self.sess.run(tf.global_variables_initializer())
+
+#        self.input = tf.placeholder(tf.uint8, [None, self.unitsize])
+        self.x = tf.placeholder(tf.float32, [None, self.unitsize])
+        print("info of self.x ", self.x)
+        self.global_step = tf.Variable(0, trainable=False, name='global_step')
+
+        self.W = tf.Variable(tf.zeros([self.unitsize, 10]))
+        self.b = tf.Variable(tf.zeros([10]))
+        self.res = tf.matmul(self.x, self.W)
+
+        self.saver = tf.train.Saver(tf.global_variables())
 
     def learn(self):
         data = self.ifile.read(self.unitsize)
+        
+        list_data = list(data)
+        """
+        list_data = list()
+        for i in list(range(self.unitsize)):
+            tmp = list(self.ifile.read(1))
+            list_data.append(tmp)
+        """
+
+        nparr = np.array(list_data)
+        nparr.reshape(1, 784)
+
+        test_data = list()
+        test_data.append(list_data)
+        test_data.append(list_data)
+        print("test_data : ", test_data)
+#        test_data.append(list_data)
+#        test_data.append(list_data)
+#        test_data.append(list_data)
+
+#        print("list size : ", len(data))
+#        print("list_data : ", list_data)
+#        self.sess.run(self.res, feed_dict={self.x: list_data})
+        self.sess.run(self.res, feed_dict={self.x: nparr})
+        self.global_step = tf.add(self.global_step, tf.constant(1))
+
+    def end(self):
+        self.saver.save(self.sess, './model/network.ckpt', global_step=self.global_step)
 
     def dump(self):
         print(self.ipath)
         print(self.lpath)
         print(int.from_bytes(self.imagic, 'big'))
-        print(int.from_bytes(self.icount, 'big'))
-        print(int.from_bytes(self.irows, 'big'))
-        print(int.from_bytes(self.icols, 'big'))
+        print(self.icount)
+        print(self.irows)
+        print(self.icols)
         print(int.from_bytes(self.lmagic, 'big'))
         print(int.from_bytes(self.lcount, 'big'))
 
@@ -45,3 +87,10 @@ a = image_learn()
 a.dump()
 
 a.tfinit()
+#a.learn()
+#a.learn()
+#a.learn()
+#a.learn()
+#a.learn()
+a.learn()
+a.end()
