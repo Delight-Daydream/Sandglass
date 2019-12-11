7ccbe016dabd3ca00b4a2bd8b120aa691bf385da
root <root@localhost.localdomain>
2019-12-11 04:30:51 -0500

    ho

--- test.py
+++ test.py
@@ -8,33 +8,31 @@ class image_learn:
         self.logpath = logpath
 
         self.ifile = open(self.ipath, "rb")
-        self.imagic = self.ifile.read(4)
+        self.imagic = int.from_bytes(self.ifile.read(4), 'big')
         self.icount = int.from_bytes(self.ifile.read(4), 'big')
         self.irows = int.from_bytes(self.ifile.read(4), 'big')
         self.icols = int.from_bytes(self.ifile.read(4), 'big')
         self.unitsize = self.irows * self.icols
 
         self.lfile = open(self.lpath, "rb")
-        self.lmagic = self.lfile.read(4)
-        self.lcount = self.lfile.read(4)
+        self.lmagic = int.from_bytes(self.lfile.read(4), 'big')
+        self.lcount = int.from_bytes(self.lfile.read(4), 'big')
 
     def tfinit(self):
 #        self.saver = tf.train.Saver(tf.global_variables())
         self.sess = tf.Session()
 
-#        self.input = tf.placeholder(tf.uint8, [None, self.unitsize])
-        self.x = tf.placeholder(tf.float32, [1, self.unitsize])
-#        self.x = tf.placeholder(tf.float32)
-        print("info of self.x ", self.x)
+        self.x = tf.placeholder(tf.float32, [None, self.unitsize])
         self.global_step = tf.Variable(0, trainable=False, name='global_step')
 
         self.W = tf.Variable(tf.zeros([self.unitsize, 10]))
         self.b = tf.Variable(tf.zeros([10]))
-        self.res = tf.matmul(self.x, self.W) + self.b
+        self.y = tf.nn.softmax(tf.matmul(self.x, self.W) + self.b)
 
         self.saver = tf.train.Saver(tf.global_variables())
 
         self.ckpt = tf.train.get_checkpoint_state('./model')
+
         if self.ckpt and tf.train.checkpoint_exists(self.ckpt.model_checkpoint_path):
             print("Load model!")
             self.saver.restore(self.sess, self.ckpt.model_checkpoint_path)
@@ -43,21 +41,19 @@ class image_learn:
             self.sess.run(tf.global_variables_initializer())
 
     def learn(self):
-        data = self.ifile.read(self.unitsize)
-        
-        _list_data = list(data)
-        list_data = list()
-        list_data.append(_list_data)
-        """
-        list_data = list()
-        for i in list(range(self.unitsize)):
-            tmp = list(self.ifile.read(1))
-            list_data.append(tmp)
-        """
+        idata = self.ifile.read(self.unitsize)
+        ldata = self.lfile.read(1)
 
-        print("list size : ", len(data))
-        print("list_data : ", list_data)
-        self.sess.run(self.res, feed_dict={self.x: list_data})
+        label_data = list(ldata)
+        print("ldata :", label_data)
+
+        _image_data = list(idata)
+        image_data = list()
+        image_data.append(_image_data)
+
+#        print("list size : ", len(data))
+#        print("image_data : ", image_data)
+        print(self.sess.run(self.y, feed_dict={self.x: image_data}))
         self.global_step = tf.add(self.global_step, tf.constant(1))
 
     def end(self):
@@ -66,12 +62,12 @@ class image_learn:
     def dump(self):
         print(self.ipath)
         print(self.lpath)
-        print(int.from_bytes(self.imagic, 'big'))
+        print(self.imagic)
         print(self.icount)
         print(self.irows)
         print(self.icols)
-        print(int.from_bytes(self.lmagic, 'big'))
-        print(int.from_bytes(self.lcount, 'big'))
+        print(self.lmagic)
+        print(self.lcount)
 
 #ifile = open("./lab/t10k-images-idx3-ubyte", "rb")
 #lfile = open("./lab/t10k-labels-idx1-ubyte", "rb")
@@ -81,10 +77,10 @@ a = image_learn()
 a.dump()
 
 a.tfinit()
-#a.learn()
-#a.learn()
-#a.learn()
-#a.learn()
-#a.learn()
+a.learn()
+a.learn()
+a.learn()
+a.learn()
+a.learn()
 a.learn()
 a.end()
